c43ed839d3778dad517716e9e7802f712a63bf13
Jiri Kvapil <kvapiji2@fit.cvut.cz>
2026-05-10 14:16:32 +0200

    fix not installing packages

--- install_packages_for_test.sh
+++ install_packages_for_test.sh
@@ -2,9 +2,8 @@
 # Install CRAN packages listed in packages_for_test.md (same order as the table).
 
 # prerequisites:
-# cd "trunk"
+# cd trunk
 # . ../scripts/config.inc
-# . ../scripts/cmpconfig.inc
 
 PACKAGES=(
 	magrittr tibble jsonlite glue curl purrr xfun utf8 htmltools
@@ -12,6 +11,13 @@ PACKAGES=(
 	ps zoo base64enc backports sys askpass rappdirs cachem bit bit64
 	uuid commonmark xts
 )
+
+if [ ! -r $RCHK/scripts/config.inc ] ; then
+  echo "Please set RCHK variables (scripts/config.inc)" >&2
+  exit 2
+fi
+
+. $RCHK/scripts/cmpconfig.inc
 
 for PACKAGE_NAME in "${PACKAGES[@]}"; do
 	echo "install.packages(\"${PACKAGE_NAME}\", repos='http://cloud.r-project.org')" | ./bin/R --no-echo
